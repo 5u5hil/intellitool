b981477efac9d3dbc631a9e1b8a63bb3398a10a8
sachin619 <echo.smb@gmail.com>
2017-02-28 16:02:11 +0530

    proposal

--- app/Http/Controllers/Admin/ProposalController.php
+++ app/Http/Controllers/Admin/ProposalController.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ProposalController extends Controller
 {
     //
+    
+    function index(){
+        $user = DB::table('test')->first();
+        echo "<pre>";
+        print_r($user);
+        exit;
+    }
 }
